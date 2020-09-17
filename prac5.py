3a6bfebd998fe3d41ec30638b42db5736bedcb1f
harshil85940 <60964254+harshil85940@users.noreply.github.com>
2020-09-17 14:13:50 +0530

    binary,linear search

--- prac5.py
+++ prac5.py
@@ -25,5 +25,5 @@ def linearsearch(arr, x):
         return i    
    return -1 
 arr = ['t','u','t','o','r','i','a','l'] 
-x = 'a' 
+x = input("enter character you want to search: ") 
 print("element found at index "+str(linearsearch(arr,x)))
