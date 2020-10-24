d5cceb415488a993b5f453d1b9a7b69efa08e1ce
harshil85940 <60964254+harshil85940@users.noreply.github.com>
2020-10-24 12:11:27 +0530

    Update practical1b.py

--- practical1b.py
+++ practical1b.py
@@ -1,47 +1,35 @@
-#addition
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[1, 2], [3, 4]]
-mat3 = [[0, 0], [0, 0]]
+Mat1 = [[3, 4, -6], 
+      [12,71,24], 
+      [21,3,21]]
 
-for i in range(0, 2):
-    for j in range(0, 2):
-        mat3[i][j] = mat1[i][j] + mat2[i][j]
+Mat2 = [[2, 16, -16],
+           [1,7,-3], 
+           [-1,3,3]]
+Mat3 = [[0,0,0,],
+     [0,0,0,],
+     [0,0,0,]]
 
-for i in range(0, 2):
-    for j in range(0, 2):
-        print(mat3[i][j], end="")
-print()
+# Matrix Addition
+for i in range(len(Mat1)):
+    for j in range(len(Mat2[0])):
+        for k in range(len(Mat2)):
+            Mat3[i][j] += Mat1[i][k] + Mat2[k][j]
+        
+print(Mat3)
 
-#multiplication
-mat1 = [[10, 9], [8, 6]]
-mat2 = [[1, 2], [3, 4]]
-mat3 = [[0, 0], [0, 0]]
+# Matrix Multiplication
 
-for i in range(0, 2):
-    for j in range(0, 2):
-        mat3[i][j] = mat1[i][j] * mat2[i][j]
+Mat3 = [[0, 0, 0, 0], 
+        [0, 0, 0, 0], 
+        [0, 0, 0, 0]] 
 
+for i in range(len(Mat1)):
+    for j in range(len(Mat2[0])):
+        for k in range(len(Mat2)):
+            Mat3[i][j] += Mat1[i][k] * Mat2[k][j]
+        
+print(Mat3)
 
-for i in range(len(mat1)):
-    for j in range(len(mat2[0])):
-        for k in range(len(mat2)):
-            mat3[i][j] =mat3[i][j] + ( mat1[i][k] * mat2[k][j])
-
-
-for r in mat3:
-    print(r)
-    
- #transpose
- X = [[12,7],
-    [4 ,5],
-    [3 ,8]]
-
-result = [[0,0,0],
-         [0,0,0]]
-
-for i in range(len(X)):
-   for j in range(len(X[0])):
-       result[j][i] = X[i][j]
-
-for r in result:
-   print(r)
+#matrix transpose
+for i in map(list, zip(*Mat1)):
+    print(i)
